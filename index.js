0ffd232879eaef5a5171b406429cb359e0e064ea
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-03-15 15:05:48 -0400

    commit tree 7eed5342c930d197428a11ebfe9894c1e3174026

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  {year: "2016", result: "N/A"}
+]
 
+function superbowlWin(record) {
+  let result = record.find(obj => obj.result === "W")
+
+  if (result) {
+    return result.year
+  } else {
+    return undefined
+  }
+}
